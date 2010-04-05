b4aa9bc0e62897eea81178108586a08f524b7e7c
Mike Dirolf <mike@10gen.com>
2010-04-05 11:25:52 -0400

    use pymongo's gridfs implementation

--- gridfs_server.py
+++ gridfs_server.py
@@ -1,49 +1,44 @@
-#!/usr/bin/env python2
-from bottle import *
+#!/usr/bin/env python
+import bottle
 import pymongo
-import mimetypes
+from gridfs import GridFS
+from gridfs.errors import NoFile
+from mimetypes import guess_type
 from itertools import imap
 from operator import itemgetter
 
 DB_NAME = 'test'
 COLLECTION_PREFIX = 'fs'
 CONNECTION = pymongo.Connection() # add host and port if needed
-SERVER = AutoServer # see bottle.py for possible servers
-DEBUG = False # don't print exceptions in http output
+SERVER = bottle.FapwsServer # see bottle.py for possible servers
+DEBUG = True # don't print exceptions in http output
 
-db = CONNECTION[DB_NAME] 
-fs = db[COLLECTION_PREFIX]
+db = CONNECTION[DB_NAME]
+fs = GridFS(db, COLLECTION_PREFIX)
 
-@get('/:filename#.*#')
+@bottle.get('/:filename#.*#')
 def serve_file(filename):
 # Most of this is based on bottle.py's static_file function but modified for GridFS
-    try: 
-        file = fs.files.find({'filename': filename}).sort('uploadDate', -1).limit(1)[0]
-    except IndexError:
-        return HTTPError(404, "Not found:" + filename)
+    try:
+        file = fs.get_last_version(filename)
+    except NoFile:
+        return bottle.HTTPError(404, "Not found:" + filename)
 
-    headers = {}
-
-    if 'contentType' in file:
-        headers['Content-Type'] = file['contentType']
-    else:
-        headers['Content-Type'] = mimetypes.guess_type(filename)[0]
-
-    headers['Content-Length'] = file['length']
-    headers['Last-Modified'] = file['uploadDate'].strftime("%a, %d %b %Y %H:%M:%S GMT")
-    headers['ETag'] = str(file['md5'])
+    content_type = file.content_type or guess_type(filename)[0]
+    headers = {'Content-Type': content_type,
+               'Content-Length': file.length,
+               'Last-Modified': file.upload_date.strftime("%a, %d %b %Y "
+                                                          "%H:%M:%S GMT"),
+               'ETag': str(file.md5)}
 
     #TODO: something nice with etag
        #return HTTPResponse("Not modified", status=304, header=header)
 
-    if request.method == 'HEAD':
-        return HTTPResponse('', header=headers)
+    if bottle.request.method == 'HEAD':
+        return bottle.HTTPResponse('', header=headers)
     else:
-        chunks = fs.chunks.find({'files_id': file['_id']}).sort('n')
-        chunks = imap(itemgetter('data'), chunks)
-        chunks = imap(str, chunks)
-        return HTTPResponse(chunks, header=headers)
+        return bottle.HTTPResponse(iter(file), header=headers)
 
 if __name__ == '__main__':
-    debug(DEBUG)
-    run(server=SERVER)
+    bottle.debug(DEBUG)
+    bottle.run(server=SERVER)
